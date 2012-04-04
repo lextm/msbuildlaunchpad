847b92a652a879c8b18c1fc13b5f8a0ea9ca04b3
lextm@sha-lexli-home <lextm@sha-lexli-home>
2012-04-04 08:11:14 +0800

    Updated deploy batch.

--- deploy.bat
+++ deploy.bat
@@ -1,8 +1,11 @@
+set msBuildDir=%WINDIR%\Microsoft.NET\Framework\v4.0.30319
+call %MSBuildDir%\msbuild msbuildlaunchpad.sln /p:Configuration=Release /p:Platform="Any CPU"
+@IF %ERRORLEVEL% NEQ 0 PAUSE
+
 mkdir .\bin
 copy psvince.dll .\bin\
 copy msbuildlaunchpad.exe.manifest .\bin\
 copy msbuildlaunchpadadmin.exe.manifest .\bin\
 copy launchpad.iss .\bin\
-cd .\bin
-copy msbuildlaunchpad.exe msbuildlaunchpadAdmin.exe
-copy msbuildlaunchpad.exe.config msbuildlaunchpadAdmin.exe.config
+
+"C:\Program Files (x86)\Inno Setup 5\iscc.exe" bin\launchpad.iss
